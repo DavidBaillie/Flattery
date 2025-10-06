56374977897fbd7b7260497b14cf3bfe4e30e3c4
DavidBaillie <DavidlBaillie@gmail.com>
2025-10-05 22:42:46 -0400

    Add project and initial tests

---
+++ test/Flattery.Tests/UnitTests/Formatters/AlphaNumericTests.cs
@@ -0,0 +1,30 @@
+﻿using Flattery.Formatters;
+
+namespace Flattery.Tests.UnitTests.Formatters;
+
+[TestFixture]
+internal class AlphaNumericTests
+{
+    [TestCase("alpha", int.MaxValue, "alpha")]
+    [TestCase("beta   ", int.MaxValue, "beta   ")]
+    [TestCase("a1pha", int.MaxValue, "a1pha")]
+    [TestCase("231123", int.MaxValue, "231123")]
+    [TestCase("1234AA", 5, "1234A")]
+    public void FormatsAlphaNumericsCorrectly(string input, int length, string output)
+    {
+        var result = AlphabeticFormatter.FormatAlphaNumericSpan(input, Convert.ToUInt32(length));
+        Assert.That(result.ToString().Equals(output, StringComparison.InvariantCulture));
+    }
+
+    [Test]
+    public void FormatIntegerThrowsInvalidOperationException()
+    {
+        Assert.Throws<InvalidOperationException>(() => AlphabeticFormatter.FormatAlphaNumericSpan(500, 10));
+    }
+
+    [Test]
+    public void FormatObjectThrowsInvalidOperationException()
+    {
+        Assert.Throws<InvalidOperationException>(() => AlphabeticFormatter.FormatAlphaNumericSpan(new object(), 10));
+    }
+}
---
+++ test/Flattery.Tests/UnitTests/Formatters/AlphabeticTests.cs
@@ -0,0 +1,29 @@
+﻿using Flattery.Formatters;
+
+namespace Flattery.Tests.UnitTests.Formatters;
+
+[TestFixture]
+internal class AlphabeticTests
+{
+    [TestCase("alpha", int.MaxValue, "alpha")]
+    [TestCase("123123beta", int.MaxValue, "beta")]
+    [TestCase("b3ta", int.MaxValue, "bta")]
+    [TestCase("alpha", 3, "alp")]
+    public void FormatAlphabeticCreatesCorrectOuput(string input, int size, string output)
+    {
+        var result = AlphabeticFormatter.FormatAlphabeticSpan(input, Convert.ToUInt32(size));
+        Assert.That(output.Equals(result.ToString(), StringComparison.InvariantCulture));
+    }
+
+    [Test]
+    public void FormatIntegerThrowsInvalidOperationException()
+    {
+        Assert.Throws<InvalidOperationException>(() => AlphabeticFormatter.FormatAlphabeticSpan(500, 10));
+    }
+
+    [Test]
+    public void FormatObjectThrowsInvalidOperationException()
+    {
+        Assert.Throws<InvalidOperationException>(() => AlphabeticFormatter.FormatAlphabeticSpan(new object(), 10));
+    }
+}
