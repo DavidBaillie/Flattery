a3f27af72662693650d059ff1fb01c958c44b23c
DavidBaillie <DavidlBaillie@gmail.com>
2025-11-18 19:23:31 -0500

    Add tests and fix formatting issue

---
+++ src/Flattery/Extensions/FormatterExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Flattery.Extensions;
+
+internal static class FormatterExtensions
+{
+    public static bool IsInterfaceType(this object source, Type targetType)
+    {
+        foreach (var i in source.GetType().GetInterfaces())
+        {
+            if (i.IsGenericType && i.GetGenericTypeDefinition() == targetType)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ test/Flattery.Tests/UnitTests/Outputs/FileGenerationTests.cs
@@ -0,0 +1,65 @@
+﻿using Flattery.Attributes;
+
+namespace Flattery.Tests.UnitTests.Outputs;
+
+[TestFixture]
+internal sealed class FileGenerationTests
+{
+    [Test]
+    public void WhenCreateSingleLineStringOutputIsCorrect()
+    {
+        var expected = "John       0180002.13Y";
+        var source = new SimpleUserExample()
+        {
+            Name = "John",
+            Age = 18,
+            BankBalance = 2.13,
+            AccountEnabled = true
+        };
+
+        var builder = new FlatFileBuilder();
+        builder.AppendRecord(source);
+
+        var output = builder.Build();
+
+        Assert.That(output, Is.EqualTo(expected),
+            message: $"Flat file builder did not generated the expected output.\nActual: [{output}]\nExpected: [{expected}]");
+    }
+
+    [Test]
+    public void WhenCreateMultiLineStringOutputIsCorrect()
+    {
+        var expected = "John       0180002.13Y\nJohn       0180002.13Y";
+        var source = new SimpleUserExample()
+        {
+            Name = "John",
+            Age = 18,
+            BankBalance = 2.13,
+            AccountEnabled = true
+        };
+
+        var builder = new FlatFileBuilder();
+        builder.AppendRecord(source);
+        builder.AppendRecord(source);
+
+        var output = builder.Build();
+
+        Assert.That(output, Is.EqualTo(expected),
+            message: $"Flat file builder did not generated the expected output.\nActual: [{output}]\nExpected: [{expected}]");
+    }
+}
+
+file class SimpleUserExample
+{
+    [AlphabeticField(0, 10)]
+    public required string Name { get; set; }
+
+    [UnsignedIntegerField<uint>(11, 13)]
+    public required uint Age { get; set; }
+
+    [FloatingPointField<double>(14, 20, DecimalDigits = 2)]
+    public required double BankBalance { get; set; }
+
+    [BooleanField(21, 21)]
+    public required bool AccountEnabled { get; set; }
+}
